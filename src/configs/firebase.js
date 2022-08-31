e7f0e696656fbf989570e722aeb1d07528ee6a74
tuantai96 <tuantai96utc@gmail.com>
2022-08-31 08:47:26 +0700

    2nd commit

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -2,15 +2,15 @@ import * as firebase from "firebase/app";
 import * as projectAuth from "firebase/auth"
 import * as firestore from "firebase/firestore";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzasdasdasdasdas",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-
+  apiKey: "AIzaSyAs9Sr-zNkD7moR5dh1FK7Vn8_DXlPa4sI",
+  authDomain: "imoney-fd9c6.firebaseapp.com",
+  projectId: "imoney-fd9c6",
+  storageBucket: "imoney-fd9c6.appspot.com",
+  messagingSenderId: "1049042816504",
+  appId: "1:1049042816504:web:27330d65bd3cbce3a29a0d",
+  measurementId: "G-F6GWELS2TE"
 };
 firebase.initializeApp(firebaseConfig);
 
